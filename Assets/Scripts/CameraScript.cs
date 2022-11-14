35a9a1c557b2e66d1c7acdf4ed098a65c7a8623f
kdh990415 <kimdae990415@gmail.com>
2022-11-14 15:26:01 +0900

    게임 시작시 카메라 무빙
    게임 시작시 카메라 무빙

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class CameraScript : MonoBehaviour
+{
+    public Camera mainCam;
+    public Camera FstCam;
+    public Camera SndCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        FstCam = GameObject.Find("1PCamera").GetComponent<Camera>();
+        SndCam = GameObject.Find("2PCamera").GetComponent<Camera>();
+
+        FstCam.enabled = false;
+        SndCam.enabled = false;
+
+        //CameraMove();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraMove();
+    }
+
+    void CameraMove()
+    {
+        if (mainCam.orthographicSize >= 12f)
+        {
+            Invoke("SecondCameraOn", 1f);
+        }
+        else
+        {
+            mainCam.orthographicSize += Time.deltaTime;
+        }
+    }
+
+    void SecondCameraOn()
+    {
+        FstCam.enabled = true;
+        SndCam.enabled = true;
+        mainCam.enabled = false;
+
+        Invoke("SecondCameraMove", 0.5f);
+
+    }
+
+    void SecondCameraMove()
+    {
+        if (FstCam.orthographicSize >= 6)
+        {
+            FstCam.orthographicSize -= (Time.deltaTime * 3f);
+            SndCam.orthographicSize -= (Time.deltaTime * 3f);
+        }
+    }
+}
